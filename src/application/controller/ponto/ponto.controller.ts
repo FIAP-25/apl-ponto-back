53aed97c95b5f6a7f7730a4132efa964afce25fc
Ricardo S. Reis <r1souzareis@gmail.com>
2024-03-24 20:28:32 -0300

    feat: atualização ponto

--- src/application/controller/ponto/ponto.controller.ts
+++ src/application/controller/ponto/ponto.controller.ts
@@ -2,23 +2,20 @@ import { AutenticacaoGuard } from '@/application/guard/autenticacao.guard';
 import { ok, unauthorized } from '@/application/helper/http.helper';
 import { IPontoUseCase } from '@/domain/contract/usecase/ponto.interface';
 import { MarcarPontoInput } from '@/infrastructure/dto/ponto/marcarPonto.dto';
-import { Body, Controller, Get, Param, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { Request, Response } from 'express';
+import { Response } from 'express';
 
 @ApiTags('Ponto Eletrônico')
 @Controller('api/ponto-eletronico')
 export class PontoController {
     constructor(private pontoUseCase: IPontoUseCase) {}
 
-    @UseGuards(AutenticacaoGuard)
     @Post('registrar')
     @ApiOperation({ summary: 'Registra uma marcação de ponto' })
-    async registrarPonto(@Body() body: MarcarPontoInput, @Res() res: Response, @Req() req: Request): Promise<any> {
-        const matricula = req.matricula;
-
-        if (matricula) {
-            const registroPonto = await this.pontoUseCase.marcarPonto(body, matricula);
+    async registrarPonto(@Body() body: MarcarPontoInput, @Res() res: Response): Promise<any> {
+        if (body) {
+            const registroPonto = await this.pontoUseCase.marcarPonto(body, body.matricula);
             return ok(registroPonto, res);
         }
         return unauthorized(res);
--- src/infrastructure/dto/ponto/marcarPonto.dto.ts
+++ src/infrastructure/dto/ponto/marcarPonto.dto.ts
@@ -4,6 +4,10 @@ import { ApiProperty } from '@nestjs/swagger';
 export class MarcarPontoInput {
     @AutoMap()
     @ApiProperty({ required: true })
+    matricula: string;
+
+    @AutoMap()
+    @ApiProperty({ required: true })
     latitude: number;
 
     @AutoMap()
